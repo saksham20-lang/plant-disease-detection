1cf732deeff49c309c2b84454af57400d2b501a3
saksham20-lang <sakshamkanungo2007@gmail.com>
2026-03-08 18:50:59 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+img_size = 128
+batch_size = 32
+
+datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+
+train = datagen.flow_from_directory(
+    "dataset",
+    target_size=(img_size,img_size),
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset="training"
+)
+
+val = datagen.flow_from_directory(
+    "dataset",
+    target_size=(img_size,img_size),
+    batch_size=batch_size,
+    class_mode="categorical",
+    subset="validation"
+)
+
+model = tf.keras.models.Sequential([
+tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(128,128,3)),
+tf.keras.layers.MaxPooling2D(2,2),
+
+tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
+tf.keras.layers.MaxPooling2D(2,2),
+
+tf.keras.layers.Flatten(),
+tf.keras.layers.Dense(128,activation='relu'),
+tf.keras.layers.Dense(train.num_classes,activation='softmax')
+])
+
+model.compile(
+optimizer='adam',
+loss='categorical_crossentropy',
+metrics=['accuracy']
+)
+
+model.fit(train,validation_data=val,epochs=5)
+
+model.save("plant_disease_model.h5")
+
+print("Model trained successfully")
